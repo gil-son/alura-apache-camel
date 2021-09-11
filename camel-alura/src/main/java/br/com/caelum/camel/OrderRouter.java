f9b1a5c58119ed9d153c92de00b8f741a5466a5d
“gil-son” <gilszon@gmail.com>
2021-09-10 21:14:04 -0300

    Send a message

--- camel-alura/src/main/java/br/com/caelum/camel/OrderRouter.java
+++ camel-alura/src/main/java/br/com/caelum/camel/OrderRouter.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class OrderRouter {
@@ -8,6 +9,20 @@ public class OrderRouter {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-
-	}	
+		context.addRoutes( new RouteBuilder() {  						// Anonymous Classes
+			
+			@Override
+			public void configure() throws Exception{
+				from("file:orders?delay=5s")
+				.log("Message ${id}")
+				.to("file:exit");
+				
+				
+			}
+		});
+		
+		context.start();
+		Thread.sleep(0);
+		context.stop();
+	}
 }
